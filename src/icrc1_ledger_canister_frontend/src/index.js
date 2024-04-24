eb7177c96757e8f94bed650c3ff61145101ab346
Sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2024-04-24 19:25:44 +0300

    Initial commit.

---
+++ src/icrc1_ledger_canister_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { icrc1_ledger_canister_backend } from "../../declarations/icrc1_ledger_canister_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await icrc1_ledger_canister_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
